bebca79558b12ae7fdc153d231ededa0923e3628
Jose - Prefeitura <molinetijose@gmail.com>
2023-02-28 16:59:34 -0300

    Commit 7 - Pref 28/02/23

--- pingteste.py
+++ pingteste.py
@@ -3,157 +3,146 @@ import threading
 import tkinter as tk
 from tkinter import simpledialog, messagebox
 import sqlite3
+from datetime import *
 
 
-data = sqlite3.connect('data_ip.db')
-cursor = data.cursor()
-
-cursor.execute("CREATE TABLE IF NOT EXISTS ips (ident integer, id string, ip string)")
-data.commit()
-data.close()
-
-def inserir_inicial():
+def main():
     data = sqlite3.connect('data_ip.db')
     cursor = data.cursor()
-    i = 1
-    while i < 35:
-        print(i)
-        aux = '192.168.' + str(i) + '.200'
-        aux1 = str(i) + '.200.'
-        print(aux)
-        print(aux1)
-        cursor.execute(f"INSERT INTO ips (ident, id, ip) VALUES(?, ?, ?)", [i, aux1, aux])
-        i = i + 1
 
+    cursor.execute("CREATE TABLE IF NOT EXISTS ips (ident integer, id string, ip string)")
     data.commit()
     data.close()
 
-
-
-
-
-ip_list = []
-
-def select():
-    try:
+    def inserir_inicial():
         data = sqlite3.connect('data_ip.db')
         cursor = data.cursor()
-        cursor.execute(f"SELECT ip FROM ips")
-        temp = cursor.fetchall()
-        i = 0
-        while i < len(temp):
-            ip_list.append(temp[i][0])
+        i = 1
+        while i < 35:
+            print(i)
+            aux = '192.168.' + str(i) + '.200'
+            aux1 = str(i) + '.200.'
+            print(aux)
+            print(aux1)
+            cursor.execute(f"INSERT INTO ips (ident, id, ip) VALUES(?, ?, ?)", [i, aux1, aux])
             i = i + 1
 
+        data.commit()
+        data.close()
 
-        print(ip_list)
+    ip_list = []
 
-        cursor.close()
+    def select():
+        try:
+            data = sqlite3.connect('data_ip.db')
+            cursor = data.cursor()
+            cursor.execute(f"SELECT ip FROM ips")
+            temp = cursor.fetchall()
+            i = 0
+            while i < len(temp):
+                ip_list.append(temp[i][0])
+                i = i + 1
 
-    except sqlite3.Error as error:
-        messagebox.showerror("Erro",error)
+            print(ip_list)
 
-select()
+            cursor.close()
 
+        except sqlite3.Error as error:
+            messagebox.showerror("Erro", error)
 
-
-def ping(ip):
-    try:
-        result = subprocess.check_output(['ping', '-n', '1', '-w', '100', ip])
-        return True
-    except subprocess.CalledProcessError:
-        return False
-
-def atualizar():
     select()
-    for ip in ip_list:
-        label = tk.Label(tela, text=ip)
-        label.pack()
-        labels.append(label)
 
-    for i, ip in enumerate(ip_list):
-        result = ping(ip)
-        cor = 'green' if result else 'red'
-        labels[i].config(bg=cor, foreground="white")
-    tela.after(180000, atualizar)
+    def ping(ip):
+        try:
+            result = subprocess.check_output(['ping', '-n', '1', '-w', '100', ip])
+            return True
+        except subprocess.CalledProcessError:
+            return False
 
-def botao_atualizar():
-    atualizar()
+    def atualizar():
+        horario = datetime.now()
+        a = tk.Label(tela, text="Atualizado - "+str(horario.hour)+":"+str(horario.minute))
+        #MUDAR O PLACE PARA PACK
+        a.place(x="200", y="10")
+        for i, ip in enumerate(ip_list):
+            result = ping(ip)
+            cor = 'green' if result else 'red'
+            labels[i].config(bg=cor, foreground="white")
+        tela.after(180000, atualizar)
 
+    def botao_atualizar():
+        atualizar()
 
-def inserir():
-    msg = simpledialog.askstring(title="Insira o IP", prompt="Insira o Final do IP - Ex: 1.200")
-    print(msg)
-    if msg == '':
-        messagebox.showerror("Erro", "Insira um IP")
-        inserir()
-    elif msg == None:
-        messagebox.showwarning("Atenção", "Nenhum IP foi inserido.")
-    else:
-        data = sqlite3.connect('data_ip.db')
-        cursor = data.cursor()
+    def inserir():
+        msg = simpledialog.askstring(title="Insira o IP", prompt="Insira o Final do IP - Ex: 1.200")
+        print(msg)
+        if msg == '':
+            messagebox.showerror("Erro", "Insira um IP")
+            inserir()
+        elif msg == None:
+            messagebox.showwarning("Atenção", "Nenhum IP foi inserido.")
+        else:
+            data = sqlite3.connect('data_ip.db')
+            cursor = data.cursor()
 
-        cursor.execute("SELECT id FROM ips")
-        aux = cursor.fetchall()
-        ident = len(aux) + 1
-        id = msg + '.'
-        ip = '192.168.' + msg
+            cursor.execute("SELECT id FROM ips")
+            aux = cursor.fetchall()
+            ident = len(aux) + 1
+            id = msg + '.'
+            ip = '192.168.' + msg
 
-        j =0
-        while j < len(aux):
-            if id == aux[j][0]:
-                messagebox.showerror("Erro", "O IP já exite na lista.")
-                return False
+            j = 0
+            while j < len(aux):
+                if id == aux[j][0]:
+                    messagebox.showerror("Erro", "O IP já exite na lista.")
+                    return False
 
+                j = j + 1
+                print(j)
 
+            if True:
+                cursor.execute(f"INSERT INTO ips (ident, id, ip) VALUES (?, ?, ?)", [ident, id, ip])
+                data.commit()
 
+                cursor.close()
 
-            j = j +1
-            print(j)
-
-        if True:
-            cursor.execute(f"INSERT INTO ips (ident, id, ip) VALUES (?, ?, ?)", [ident, id, ip])
-            data.commit()
+                print(ip_list)
+                messagebox.showinfo("Sucesso", "IP " + ip + " inserido com sucesso!")
+                reset()
 
 
             cursor.close()
 
-            print(ip_list)
-            messagebox.showinfo("Sucesso", "IP " + ip + " inserido com sucesso!")
-            atualizar()
+    def reset():
+        tela.destroy()
+        main()
 
+    tela = tk.Tk()
+    tela.title("Ip - Verificar")
+    tela.geometry("300x850")
 
-
-
-
-        cursor.close()
-
-
-
-
-
-        #ARRUMAR AQUI ------ FAZER A CONDICIONAL PARA CASO NÃO TENHA ERRO, INSERIR NO BANCO
-
-def test():
-    tela.destroy()
-
-
-tela = tk.Tk()
-tela.title("Ip - Verificar")
-tela.geometry("200x800")
-labels = []
-for ip in ip_list:
-    label = tk.Label(tela, text=ip)
-    label.pack()
+    labels = []
+    for ip in ip_list:
+        label = tk.Label(tela, text=ip)
+        label.pack()
     labels.append(label)
 
-button = tk.Button(tela, text='Atualizar', command=botao_atualizar)
-button.pack(padx=10, pady=10)
 
-buttonA = tk.Button(tela, text='Inserir', command=inserir)
-buttonA.pack()
+    button = tk.Button(tela, text='Atualizar', command=botao_atualizar)
+    button.pack(padx=10, pady=10)
 
-atualizar_label = threading.Thread(target=atualizar)
-atualizar_label.start()
+    buttonA = tk.Button(tela, text='Inserir', command=inserir)
+    buttonA.pack()
 
-tela.mainloop()
+    atualizar_label = threading.Thread(target=atualizar)
+    atualizar_label.start()
+
+    tela.mainloop()
+
+
+
+data = datetime.now()
+
+
+
+main()
